d419c8a559768901b4d110d31e97dd0d5020a862
Ray Villalobos <ray@planetoftheweb.com>
2015-11-10 18:36:35 -0500

    pulled files out of organized folders so it works with github

--- src/assets/js/search.js
+++
@@ -1,6 +0,0 @@
-var searchApp = angular.module('searchApp', [])
-	.controller('searchControllers', ['$scope', '$http', function($scope, $http) {
-  $http.get('/feeds/feed.json').success(function(data) {
-    $scope.results = data;
-  });
-}]);
