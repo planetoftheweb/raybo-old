de2da1bc35326cf87fc2bc8b36af422994afc419
Ray Villalobos <ray@planetoftheweb.com>
2020-07-10 15:36:17 -0400

    moving _process outside _layouts (not really a layout)

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,10 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   entry: [
-    "./_site/_templates/_process/js/script.js",
-    "./_site/_templates/_process/scss/style.scss",
-    "./_site/_templates/_process/scss/_custom.scss"
-  ]
-};
+    './_site/_process/js/script.js',
+    './_site/_process/scss/style.scss',
+    './_site/_process/scss/_custom.scss',
+  ],
+}
